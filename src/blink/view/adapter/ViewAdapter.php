503aa9fc68ecc9ce88568dab4fbd8d0400bbbf68
Tony Lu <lucups@gmail.com>
2015-10-24 04:41:04 +0000

    init project

---
+++ src/blink/view/adapter/ViewAdapter.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright (c) 2015 Tony Lu, All rights reserved.
+ * Author: Tony Lu <lucups@live.com>
+ * Create: 15/10/24 12:23
+ */
+
+namespace blink\view\adapter;
+
+/**
+ * Class ViewAdapter
+ * @package blink\view\adapter
+ */
+abstract class ViewAdapter implements AdapterInterface
+{
+    public $isDebug;
+
+    protected $_viewsDir;
+    protected $_cacheDir;
+
+    public function isDebug()
+    {
+        return $this->isDebug;
+    }
+
+    public function getViewsDir()
+    {
+        return $this->_viewsDir;
+    }
+
+    public function setViewsDir($viewsDir)
+    {
+        $this->$viewsDir = $viewsDir;
+        return $this;
+    }
+
+    public function getCacheDir()
+    {
+    }
+
+    public function setCacheDir($cacheDir)
+    {
+        $this->_cacheDir = $cacheDir;
+        return $this;
+    }
+
+    public function render()
+    {
+    }
+}
